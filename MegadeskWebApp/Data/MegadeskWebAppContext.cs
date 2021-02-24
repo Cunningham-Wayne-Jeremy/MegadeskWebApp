2ee6d804e07aee2d17d03b4dca48f4e41d4f09f3
Cara Batty <caradevey@yahoo.com>
2021-02-23 20:55:05 -0700

    Added DeskQuote class, scaffolding, and database

---
+++ MegadeskWebApp/Data/MegadeskWebAppContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MegadeskWebApp.Models;
+
+namespace MegadeskWebApp.Data
+{
+    public class MegadeskWebAppContext : DbContext
+    {
+        public MegadeskWebAppContext (DbContextOptions<MegadeskWebAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MegadeskWebApp.Models.DeskQuote> DeskQuote { get; set; }
+    }
+}
---
+++ MegadeskWebApp/Pages/DeskQuotes/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MegadeskWebApp.Data;
+using MegadeskWebApp.Models;
+
+namespace MegadeskWebApp.Pages.DeskQuotes
+{
+    public class IndexModel : PageModel
+    {
+        private readonly MegadeskWebApp.Data.MegadeskWebAppContext _context;
+
+        public IndexModel(MegadeskWebApp.Data.MegadeskWebAppContext context)
+        {
+            _context = context;
+        }
+
+        public IList<DeskQuote> DeskQuote { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            DeskQuote = await _context.DeskQuote.ToListAsync();
+        }
+    }
+}
